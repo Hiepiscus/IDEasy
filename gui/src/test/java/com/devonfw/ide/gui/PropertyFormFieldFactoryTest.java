c5bdd19e1ce638453fca139b980ce7844343408c
Hiepiscus <le-hiep.ha@capgemini.com>
2026-08-11 08:35:00 +0200

    #2240: add test for folderProperty form field

--- gui/src/test/java/com/devonfw/ide/gui/PropertyFormFieldFactoryTest.java
+++ gui/src/test/java/com/devonfw/ide/gui/PropertyFormFieldFactoryTest.java
@@ -2,6 +2,8 @@ package com.devonfw.ide.gui;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -10,6 +12,7 @@ import javafx.scene.layout.HBox;
 import org.junit.jupiter.api.Test;
 
 import com.devonfw.tools.ide.property.BooleanProperty;
+import com.devonfw.tools.ide.property.FolderProperty;
 import com.devonfw.tools.ide.property.KeywordProperty;
 import com.devonfw.tools.ide.property.StringProperty;
 
@@ -19,21 +22,21 @@ public class PropertyFormFieldFactoryTest extends HeadlessApplicationTest {
   @Test
   void keywordPropertyShouldProduceLabel() {
     KeywordProperty prop = new KeywordProperty("install", false, null);
-    javafx.scene.Node node = PropertyFormFieldFactory.createFormField(prop, null);
+    javafx.scene.Node node = PropertyFormFieldFactory.createFormField(prop, null, null);
     assertThat(node).isInstanceOf(Label.class);
   }
 
   @Test
   void booleanPropertyShouldProduceCheckBox() {
     BooleanProperty prop = new BooleanProperty("--force", false, "-f");
-    javafx.scene.Node node = PropertyFormFieldFactory.createFormField(prop, null);
+    javafx.scene.Node node = PropertyFormFieldFactory.createFormField(prop, null, null);
     assertThat(node).isInstanceOf(CheckBox.class);
   }
 
   @Test
   void stringPropertyShouldProduceHBoxWithLabelAndTextField() {
     StringProperty prop = new StringProperty("--name", true, null);
-    javafx.scene.Node node = PropertyFormFieldFactory.createFormField(prop, null);
+    javafx.scene.Node node = PropertyFormFieldFactory.createFormField(prop, null, null);
 
     assertThat(node).isInstanceOf(HBox.class);
     HBox hbox = (HBox) node;
@@ -42,4 +45,19 @@ public class PropertyFormFieldFactoryTest extends HeadlessApplicationTest {
     assertThat(hbox.getChildrenUnmodifiable().get(0)).isInstanceOf(Label.class);
     assertThat(hbox.getChildrenUnmodifiable().get(1)).isInstanceOf(TextField.class);
   }
+
+  @Test
+  void folderPropertyShouldProduceHBoxWithLabelTextFieldAndBrowseButton() {
+    FolderProperty prop = new FolderProperty("--target", true, null, false);
+    Node node = PropertyFormFieldFactory.createFormField(prop, null, null);
+
+    assertThat(node).isInstanceOf(HBox.class);
+    HBox hbox = (HBox) node;
+
+    assertThat(hbox.getChildren()).hasSize(3);
+    assertThat(hbox.getChildren().get(0)).isInstanceOf(Label.class);
+    assertThat(hbox.getChildren().get(1)).isInstanceOf(TextField.class);
+    assertThat(hbox.getChildren().get(2)).isInstanceOf(Button.class);
+  }
+
 }
